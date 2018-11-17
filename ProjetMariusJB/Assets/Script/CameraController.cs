5b0f1a20aadfac63945f4f8449ca0021dd4c14f4
Marius Saurel <marius.saurel@hotmail.fr>
2018-11-17 03:57:34 +0100

    Debut projet

---
+++ ProjetMariusJB/Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private GameObject PlayerModel;
+
+    private Vector3 offset;
+
+
+	void Start () {
+        offset = transform.position - PlayerModel.transform.position;
+	}
+	
+
+	void LateUpdate () {
+        transform.position = PlayerModel.transform.position + offset;
+	}
+}
---
+++ ProjetMariusJB/Assets/Script/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed;
+    public GameObject Player;
+    public Camera cam;
+
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        Vector3 difference = Input.mousePosition - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+    }
+    //Gère tout ce qui se passe avant une colision
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        Vector2 mouseposition = Input.mousePosition;
+
+    }
+}
